a4f2d07bf1ea9e330f9b7e9c3e7fb3cf0b308cf7
asalivonov <asalivonov@gmail.com>
2021-07-22 16:18:24 +0300

    Better message for unknown command

--- src/main/java/command/UnknownCommand.java
+++ src/main/java/command/UnknownCommand.java
@@ -4,7 +4,7 @@ public class UnknownCommand implements IRunableCommand{
 
 	@Override
 	public boolean run() {
-		System.out.println("ERROR Command is unknown!");
+		System.out.println("ERROR Command is unknown! Please use following commands: \n del \n ins \n save \n quit");
 		return true;
 	}
 
